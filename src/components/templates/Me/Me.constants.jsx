24714c8938e9a45c7462242d5838e6928e36a95f
Zach Kaigler <zachkaiglerfilm@gmail.com>
2023-08-28 11:32:26 -0400

    tweaked bio

--- src/components/templates/Me/Me.constants.jsx
+++ src/components/templates/Me/Me.constants.jsx
@@ -8,10 +8,10 @@ export const data = {
       </p>
       <p>
           I'm currently working with Glen Lake Pioneering to help companies transform their
-          ideas into fantastic products, and so I'm not currently looking for new opportunites.
+          ideas into fantastic products, and so at the moment I'm not looking for new opportunites.
           I am, however, always up for talking shop! Please feel free to reach out with any thoughts,
           questions, or Magic: The Gathering deck ideas.
       </p>
     </>
   )
-}
+};
