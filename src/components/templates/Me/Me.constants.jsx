a4ee514b1f32ef5d438fd28c7d945e97ecdab26c
Zach Kaigler <zachkaiglerfilm@gmail.com>
2025-03-02 10:26:58 -0500

    fix typo

--- src/components/templates/Me/Me.constants.jsx
+++ src/components/templates/Me/Me.constants.jsx
@@ -9,7 +9,7 @@ export const data = {
       <p>
           I'm currently working with <a href='https://capsule.video'>Capsule</a> to develop AI powered
           tools built to help democratize video production for everyone, and so at the moment I'm not
-          looking for new opportunites. I am, however, always up for talking shop! Please feel free to
+          looking for new opportunities. I am, however, always up for talking shop! Please feel free to
           reach out with any thoughts, questions, or Magic: The Gathering deck ideas.
       </p>
     </>
